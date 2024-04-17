b993361c35f4b5d8e68b3050a7000cd01aa9791b
Gennady <115860397+gl129@users.noreply.github.com>
2024-04-17 15:16:11 +0300

    Add files via upload

--- action_plugins/mikrotik.py
+++ action_plugins/mikrotik.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# Copyright: (c) 2022-2024, Gennady Lapin <gennady.lapin@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 import copy
 import itertools
 import re
